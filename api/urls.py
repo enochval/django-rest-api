8ba9dd01b92c9f28fbd53b5262429acc0f1cb97b
Enoch Osarenren <osarenrenenoch@gmail.com>
2026-01-17 20:06:27 +0100

    added a non model serializer

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from api import views
 
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
+    path('products/info/', views.product_info, name='product-info'),
     path('products/<int:pk>/', views.get_product, name='get-product'),
     path('orders/', views.order_list, name='order-list')
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import Product, Order
-from api.serializers import ProductSerializer, OrderSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -21,4 +22,15 @@ def get_product(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        "products": products,
+        "count": products.count(),
+        "max_price": products.aggregate(max_price=Max('price'))['max_price'] or 0
+    })
     return Response(serializer.data)
