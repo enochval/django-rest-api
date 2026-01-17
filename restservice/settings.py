45a428e408bead7424ebcb56d8a684846050e3d9
Enoch Osarenren <osarenrenenoch@gmail.com>
2026-01-17 16:55:35 +0100

    initial setups and modeling

--- restservice/settings.py
+++ restservice/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
