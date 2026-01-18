4067e9dc59d708f7f9d1e9ea755b1537b8be6459
Enoch Osarenren <osarenrenenoch@gmail.com>
2026-01-18 19:38:51 +0100

    switched functional api view to class based APIView

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view(), name='product-list'),
-    path('products/info/', views.product_info, name='product-info'),
+    path('products/info/', views.ProductInfoAPIView.as_view(), name='product-info'),
     path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='get-product'),
     path('orders/', views.OrderListAPIView.as_view(), name='order-list'),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-order-list')
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from api.models import Product, Order
@@ -30,13 +31,12 @@ class UserOrderListAPIView(generics.ListAPIView):
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
 
-
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        "products": products,
-        "count": products.count(),
-        "max_price": products.aggregate(max_price=Max('price'))['max_price'] or 0
-    })
-    return Response(serializer.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            "products": products,
+            "count": products.count(),
+            "max_price": products.aggregate(max_price=Max('price'))['max_price'] or 0
+        })
+        return Response(serializer.data)
